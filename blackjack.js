0456ff73ce2ced7808f269baf19771ac9cf775cc
restodruid1 <106450663+restodruid1@users.noreply.github.com>
2025-03-04 13:45:50 -0800

    Hit and Stay Buttons

--- blackjack.js
+++ blackjack.js
@@ -1,3 +1,8 @@
+let playerValue = 0;
+let dealerValue = 0;
+let hiddenCard = ""
+let playerBusted = false;
+
 window.onload = function() {
     buildDeck();
     shuffleDeck();
@@ -14,7 +19,7 @@ function dealCard () {
 
 async function showCard(numPlayers) {
     for (let i = 0; i < numPlayers*2; i++) {
-        dealCard();
+        //dealCard();
         await delay(1000);
         let card = document.createElement("img");
         card.src = "images/cards/" + cards[i] + ".png";
@@ -24,8 +29,10 @@ async function showCard(numPlayers) {
             document.getElementById("player-cards").append(card);
         }
         else {  // This is the dealers cards
-            console.log(Math.floor(i/2));
+            //console.log(Math.floor(i/2));
             if (Math.floor(i/2) == 0) {
+                hiddenCard = card.src;
+                console.log(hiddenCard);
                 card.src = "images/cards/BACK.png";
                 document.getElementById("dealer-cards").append(card);
             }
@@ -35,21 +42,22 @@ async function showCard(numPlayers) {
             
         }   
     }
-    //document.getElementById("hit").style.display = "inline-block";
 }
     
 
 function playGame() {
     while (true) {
-        let playerValue = 0;
-        let dealerValue = 0;
-
-        cards = [];  
+        cards = [];
+        dealCard();
+        dealCard();
+        dealCard();
+        dealCard();  
         showCard(2);
-        //playerValue += getValue(cards[0]) + getValue(cards[2]);
-        //dealerValue += getValue(cards[1]) + getValue(cards[3]);
-
+        playerValue += getValue(cards[0]) + getValue(cards[2]);
+        dealerValue += getValue(cards[1]) + getValue(cards[3]);
+        console.log(playerValue, dealerValue);
         showButtons();
+        
         break;
     }
 }
@@ -60,9 +68,9 @@ async function showButtons () {
     hitVar.style.display = "inline-block";
     hitVar.addEventListener("click", hit);
 
-    let stay = document.getElementById("stay"); 
-    stay.style.display = "inline-block";
-    stay.addEventListener("click", stay);
+    let stayVar = document.getElementById("stay"); 
+    stayVar.style.display = "inline-block";
+    stayVar.addEventListener("click", stay);
 }
 
 function hit() {
@@ -70,11 +78,87 @@ function hit() {
     let card = document.createElement("img");
     card.src = "images/cards/" + cards.at(-1) + ".png";
     document.getElementById("player-cards").append(card);
-
+    playerValue += getValue(cards.at(-1));
+    if (playerValue > 21) {
+        console.log("Player Busted");
+        playerBusted = true;
+        dealerLogic();
+    }
+    console.log(playerValue);
 }
 function stay() {
-
+    // Player stays, run dealer logic
+    dealerLogic();   
 }
+
+function resetGame() {
+    document.getElementById("dealer").innerHTML = "";
+    document.getElementById("dealer").innerHTML = "<h2 id=\"dealer-cards\"></h2>";
+    document.getElementById("player").innerHTML = "";
+    document.getElementById("player").innerHTML = "<h2 id=\"player-cards\"></h2>";
+    document.getElementById("winner").innerHTML = "";
+    if (deck.length < 10) {
+        console.log("Shuffling New Deck");
+        buildDeck();
+        shuffleDeck();
+    }
+    playerBusted = false;
+    playerValue = 0;
+    dealerValue = 0;
+    playGame();
+}
+
+async function dealerLogic() {
+    let hidden = document.getElementById("dealer-cards");
+    //console.log(hidden);
+    hidden.firstChild.src = hiddenCard;
+    await delay(2000);
+    //console.log(hidden);
+    if (playerBusted == false) {    // Dealer hits 
+        while (dealerValue < 17) {
+            //await delay(2000);
+            dealCard();
+            let card = document.createElement("img");
+            card.src = "images/cards/" + cards.at(-1) + ".png";
+            document.getElementById("dealer-cards").append(card);
+            dealerValue += getValue(cards.at(-1));
+            console.log(dealerValue);
+            await delay(2000);
+        }
+        //if (dealerValue > 21) {
+        //    console.log("dealer busted");
+        //}
+    }
+    //else {
+    //    console.log("player lost");
+    //}
+    compareValues()
+    resetGame();
+}
+
+async function compareValues() {
+    //let result = document.getElementById("winner");
+
+    if (playerBusted) {
+        //result.innerText = "You Lost";
+        document.getElementById("winner").innerText = "You Lost";
+    }
+    else if (dealerValue > 21) {
+        result.innerText = "You Won";
+    }
+    else if (playerValue > dealerValue) {
+        result.innerText = "You Won";
+    }
+    else if (dealerValue > playerValue) {
+        result.innerText = "You Lost";
+    } 
+    else {
+        result.innerText = "You Push";
+    }
+
+    await delay(3000);
+}
+
 function getValue(card) {
     let data = card.split("-"); // "4-C" -> ["4", "C"]
     let value = data[0];
